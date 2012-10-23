6fb3f1f4aa04ec274e2953e89a36cba3bed22723
Chris van de Steeg <chris@chrisvandesteeg.nl>
2012-10-23 16:20:49 +0200

    Conditional MVC building

--- Source/Glass.Sitecore.Mapper/Pipelines/Response/GetModel.cs
+++ Source/Glass.Sitecore.Mapper/Pipelines/Response/GetModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿#if !preMVC
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -48,3 +49,4 @@ namespace Glass.Sitecore.Mapper.Pipelines.Response
         }
     }
 }
+#endif
