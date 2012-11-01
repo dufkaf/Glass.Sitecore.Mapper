6583364a140fcc4028697f6b29a338146c39f4c7
Chris van de Steeg <chris@chrisvandesteeg.nl>
2012-11-01 17:35:58 +0100

    forgot the class

---
+++ Source/Glass.Sitecore.Mapper/Configuration/Attributes/SitecoreFieldFieldAttribute.cs
@@ -0,0 +1,55 @@
+﻿/*
+   Copyright 2011 Michael Edwards
+ 
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ 
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Glass.Sitecore.Mapper.Data;
+
+namespace Glass.Sitecore.Mapper.Configuration.Attributes
+{
+    /// <summary>
+    /// Used to populate default values of a field's field
+    /// </summary>
+    public class SitecoreFieldFieldValueAttribute : Attribute
+    {
+        /// <summary>
+        /// Indicates that the property should pull data from a Sitecore field.
+        /// </summary>
+        /// <param name="fieldId">The Id (Guid) of the field to load</param>
+        /// <param name="fieldValue">The default field value </param>
+        public SitecoreFieldFieldValueAttribute(string fieldId, string fieldValue)
+        {
+            FieldValue = fieldValue;
+#if NET40
+            FieldId = Guid.Parse(fieldId);
+#else
+            FieldId = new Guid(fieldId);
+#endif
+        }
+
+        /// <summary>
+        /// The Id (Guid) of the field to load
+        /// </summary>
+        public Guid FieldId { get; set; }
+
+        /// <summary>
+        /// The title for the field if using Code First
+        /// </summary>
+        public string FieldValue { get; set; }
+    }
+}
